4177a108c6e903744e03146806570c6b0d459e43
diamond-one <thisismattdiamond@gmail.com>
2023-11-11 18:33:09 +0100

    Coloured title

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class Colors:
     RED = '\033[91m'
     GREEN = '\033[92m'
     BLUE = '\033[94m'
+    WHITE = '\033[97m'  
     RESET = '\033[0m'
 
 win = True
--- utilities/title.py
+++ utilities/title.py
@@ -1,3 +1,10 @@
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    BLUE = '\033[94m'
+    WHITE = '\033[97m'
+    RESET = '\033[0m'
+
 def print_title_art():
     title = """
  ██████╗███████╗███████╗ ██████╗██╗  ██╗     ██████╗ ██╗   ██╗███████╗███████╗████████╗
@@ -7,4 +14,16 @@ def print_title_art():
 ╚██████╗███████╗███████╗╚██████╗██║  ██║    ╚██████╔╝╚██████╔╝███████╗███████║   ██║   
  ╚═════╝╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝     ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝   
     """
+
+    # Replace characters in the title with corresponding colors
+   #  title = title.replace('█', Colors.RED + '█' + Colors.RESET)
+    title = title.replace('╗', Colors.BLUE + '╗' + Colors.RESET)
+    title = title.replace('╝', Colors.BLUE + '╝' + Colors.RESET)
+    title = title.replace('═', Colors.RED + '═' + Colors.RESET)
+    title = title.replace('╚', Colors.RED + '╚' + Colors.RESET)
+    title = title.replace(' ', Colors.BLUE + ' ' + Colors.RESET)
+    title = title.replace('║', Colors.BLUE + '║' + Colors.RESET) 
+    title = title.replace('╔', Colors.BLUE + '╔' + Colors.RESET)
+
     print(title)
+
